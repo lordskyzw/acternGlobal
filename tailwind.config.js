8275e2e322821a69b8092698aae1cede1b9d27e1
Tarmica <68661064+lordskyzw@users.noreply.github.com>
2023-01-17 15:55:06 +0200

    enter tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html, js}', './src/**/*.vue', './src/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
